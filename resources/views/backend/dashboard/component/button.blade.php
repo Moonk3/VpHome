90dacee225783d5d45a5afae9b10d3744d9decfe
Moonk3 <thptdaian.levanminhk1821@gmail.com>
2025-04-04 22:19:19 +0700

    next

--- resources/views/backend/dashboard/component/button.blade.php
+++ resources/views/backend/dashboard/component/button.blade.php
@@ -1,4 +1,4 @@
 <div class="text-right mb15 fixed-bottom">
-    <button class="btn btn-primary" type="submit" name="code" value="send">{{ __('messages.save') }}</button>
     {{-- <button class="btn btn-primary" type="submit" name="send" value="send">{{ __('messages.save') }}</button> --}}
+    <button class="btn btn-primary" type="submit" name="code" value="code">{{ __('messages.save') }}</button>
 </div>
