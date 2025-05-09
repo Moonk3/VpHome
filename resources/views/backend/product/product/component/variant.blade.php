dc73878ab4447741231fadabc2b2530fa987eea5
Moonk3 <thptdaian.levanminhk1821@gmail.com>
2025-05-09 13:02:39 +0700

    finally

--- resources/views/backend/product/product/component/variant.blade.php
+++ resources/views/backend/product/product/component/variant.blade.php
@@ -1,4 +1,4 @@
-{{-- 
+
 <div class="ibox variant-box">
     <div class="ibox-title">
         <div>
@@ -109,4 +109,4 @@
     var attribute = '{{ base64_encode(json_encode(old('attribute') ?? (isset($product->attribute) ? $product->attribute : []))) }}';
     var variant = '{{ base64_encode(json_encode(old('variant') ?? (isset($product->variant) ? json_decode($product->variant, TRUE) : []))) }}';
 
-</script> --}}
+</script>
